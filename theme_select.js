3233397a1421a0097503e9b71969a27413c90ed8
l0drex <sunshine.lorenz@web.de>
2020-04-14 14:02:29 +0200

    Fix schedule

--- theme_select.js
+++ theme_select.js
@@ -1,14 +1,17 @@
 /*
 On startup, connect to the "yin_yang" app.
 */
-var time_day, time_night;
-var theme_light = "firefox-compact-light@mozilla.org";
-var theme_dark = "firefox-compact-dark@mozilla.org";
+var schedule = false;
+var time_day = new Object(), time_night = new Object();
+var theme_light = "firefox-compact-light@mozilla.org",
+    theme_dark = "firefox-compact-dark@mozilla.org";
     
 function checkTime() {
+  console.log("Check time");
   date = new Date();
   hours = date.getHours();
-  if ((hours >= time_day) && (hours < time_night)) {
+  minutes = date.getMinutes();
+  if ((hours >= time_day[0]) && (minutes >= time_day[1]) && (hours < time_night[0]) && (minutes >= time_day[1])) {
     browser.management.setEnabled(theme_light, true);
   } else {
     browser.management.setEnabled(theme_dark, true);
@@ -16,23 +19,22 @@ function checkTime() {
 }
 
 function onResponse(response) {
-  var theme_active = ''
+  var theme_active = '';
   if (response.schedule) {
     time_day = response.time_day;
     time_night = response.time_night;
     theme_light = response.theme_light;
     theme_dark = response.theme_dark;
-    console.log("Time Day: " + response.time_day);
-    console.log("Time Night: " + time_night);
+    console.log("Time Day: " + time_day[0] + ":" + time_day[1]);
+    console.log("Time Night: " + time_night[0] + ":" + time_night[1]);
     console.log("Theme Light: " + theme_light);
     console.log("Theme Dark: " + theme_dark);
 
     checkTime();
-    browser.alarms.create('checkTime', {periodInMinutes: 2});
   } else {
     theme_active = response.theme_active;
     browser.management.setEnabled(theme_active, true);
-    console.log(`Switched to theme ${theme_active}`)
+    console.log(`Switched to theme ${theme_active}`);
   }
 }
 
@@ -42,8 +44,12 @@ function onError(error) {
 
 
 // Check settings from yin_yang
-console.log("Check settings.")
+console.log("Check settings.");
 var sending = browser.runtime.sendNativeMessage("yin_yang", "GetSettings");
 sending.then(onResponse, onError);
+
+if (schedule) {
+    browser.alarms.create('checkTime', {periodInMinutes: 2});
+}
 
 browser.alarms.onAlarm.addListener(checkTime);
