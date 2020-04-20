71a3463a7b7cdaefc7707cba6c7df488d474784c
l0drex <sunshine.lorenz@web.de>
2020-04-20 13:40:19 +0200

    Add more logs which strangly fixed schedule issues

--- theme_select.js
+++ theme_select.js
@@ -7,39 +7,41 @@ var theme_light = "firefox-compact-light@mozilla.org",
     theme_dark = "firefox-compact-dark@mozilla.org";
     
 function checkTime() {
-  console.log("Check time");
-  date = new Date();
-  hours = date.getHours();
-  minutes = date.getMinutes();
-  if ((hours >= time_day[0]) && (minutes >= time_day[1]) && (hours < time_night[0]) && (minutes >= time_day[1])) {
-    browser.management.setEnabled(theme_light, true);
-  } else {
-    browser.management.setEnabled(theme_dark, true);
-  }
+    date = new Date();
+    hours = date.getHours();
+    minutes = date.getMinutes();
+    var time = hours + ":" + minutes
+    if ((hours >= time_day[0]) && (minutes >= time_day[1]) && (hours < time_night[0]) && (minutes >= time_day[1])) {
+        console.log(time + ": Enable light theme")
+        browser.management.setEnabled(theme_light, true);
+    } else {
+        console.log(time + ": Enable dark theme")
+        browser.management.setEnabled(theme_dark, true);
+    }
 }
 
 function onResponse(response) {
-  var theme_active = '';
-  if (response.schedule) {
-    time_day = response.time_day;
-    time_night = response.time_night;
-    theme_light = response.theme_light;
-    theme_dark = response.theme_dark;
-    console.log("Time Day: " + time_day[0] + ":" + time_day[1]);
-    console.log("Time Night: " + time_night[0] + ":" + time_night[1]);
-    console.log("Theme Light: " + theme_light);
-    console.log("Theme Dark: " + theme_dark);
+    var theme_active = '';
+    if (response.schedule) {
+        time_day = response.time_day;
+        time_night = response.time_night;
+        theme_light = response.theme_light;
+        theme_dark = response.theme_dark;
+        console.log("Time Day: " + time_day[0] + ":" + time_day[1]);
+        console.log("Time Night: " + time_night[0] + ":" + time_night[1]);
+        console.log("Theme Light: " + theme_light);
+        console.log("Theme Dark: " + theme_dark);
 
-    checkTime();
-  } else {
-    theme_active = response.theme_active;
-    browser.management.setEnabled(theme_active, true);
-    console.log(`Switched to theme ${theme_active}`);
-  }
+        checkTime();
+    } else {
+        theme_active = response.theme_active;
+        browser.management.setEnabled(theme_active, true);
+        console.log(`Switched to theme ${theme_active}`);
+    }
 }
 
 function onError(error) {
-  console.log(`Error: ${error}`);
+    console.log(`Error: ${error}`);
 }
 
 
