b76ceb81ef8550ba932ba221c3c97bd7ee446da8
l0drex <l0drex@pm.me>
2021-03-08 23:40:54 +0100

    More fixes

--- theme_select.js
+++ theme_select.js
@@ -3,12 +3,7 @@ var times = [0, 1];
 
 function setTheme(dark_mode) {
     console.log((dark_mode ? 'Enabling' : 'Disabling')+ ' dark mode');
-    let theme;
-    if (dark_mode) {
-        theme = themes[1];
-    } else {
-        theme = themes[0];
-    }
+    let theme = themes[dark_mode ? 1 : 0];
 
     if (!browser.management.get(theme).enabled) {
         browser.management.setEnabled(theme, true);
@@ -30,53 +25,54 @@ function shouldBeDark(time_light, time_dark) {
     }
 }
 
+function changeTheme() {
+    let dark_mode = shouldBeDark()
+    // set appropriate theme
+    setTheme(dark_mode)
+    // increase time for next alarm by on day
+    console.debug('old time: ' + times[dark_mode ? 1 : 0]);
+    times[dark_mode ? 1 : 0] += 60 * 60 * 24;
+    console.debug('new time: ' + times[dark_mode ? 1 : 0]);
+
+    // create an alarm
+    browser.alarms.clearAll();
+    const when = times[dark_mode ? 1 : 0];
+    console.debug('Creating alarm at ' + when);
+    browser.alarms.create("auto_dark_mode", {
+        when
+    });
+
+    // check that it was successful
+    browser.alarms.get("auto_dark_mode").then((alarm) => {
+        console.debug(themes[!dark_mode ? 1 : 0] + " will be activated at " + alarm.scheduledTime);
+    }, () => {
+        console.error("Alarm was not created correctly.");
+    });
+}
+
 function onResponse(response) {
+    console.debug('received response')
     if (!response.enabled) {
         console.log("Yin Yang is disabled")
         return;
     }
 
     themes = response.themes;
-    setTheme(response.dark_mode);
 
     if (!response.scheduled) {
         console.log("Automatic theme switching is disabled");
+        setTheme(response.dark_mode);
         return;
     }
 
-    // if the theme should change automatically
+    // if the theme should change automatically:
     times = response.times;
-    for (let i of [0, 1]) { // 1: dark_mode
-        // create an alarm
-        const when = times[i];
-        const periodInMinutes = 60 * 24;
+    changeTheme();
 
-        console.debug('Creating alarm at ' + when + ' for every ' + periodInMinutes);
-        browser.alarms.create("alarm_" + i, {
-            when,
-            periodInMinutes
-        });
-
-        // check that it was successful
-        browser.alarms.get("alarm_" + i).then((alarm) => {
-            console.debug(themes[i] + " will be activated at " + alarm.scheduledTime);
-        }, () => {
-            console.error("Alarm was not created correctly.");
-        });
-    }
-
-    // add listener
+    // add listener for alarms
     browser.alarms.onAlarm.addListener((alarm) => {
-        console.debug("Alarm: " + alarm.name);
-        switch (alarm.name) {
-            case "alarm_0":
-                setTheme(false);
-                break;
-            case "alarm_1":
-                setTheme(true);
-                break;
-            default:
-                break;
+        if (alarm.name === "auto_dark_mode") {
+            changeTheme();
         }
     });
 }
