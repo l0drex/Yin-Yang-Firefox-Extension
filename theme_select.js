d855e27bc9561431f558fa509f1bf737eefa2453
l0drex <l0drex@pm.me>
2021-03-09 12:23:40 +0100

    Updated some names

--- theme_select.js
+++ theme_select.js
@@ -4,10 +4,10 @@ let times = [0, 1];
 function setTheme(dark) {
     let theme = themes[dark ? 1 : 0];
 
-    browser.management.get(theme).then((theme) => {
+    browser.management.get(theme).then(theme => {
         if (!theme.enabled) {
             browser.management.setEnabled(theme, true);
-            console.debug(`Switched to theme ${theme}`);
+            console.log(`Switched to theme ${theme}`);
         }
     }, () => {
         console.error('Theme is not installed.')
@@ -15,7 +15,6 @@ function setTheme(dark) {
 }
 
 function shouldBeDark(time_light, time_dark) {
-    // get current time
     let time_current = Date.now();
 
     // if light is activated first
@@ -28,16 +27,16 @@ function shouldBeDark(time_light, time_dark) {
     }
 }
 
-function changeTheme() {
+function update_alarm() {
+    // get currently used mode
     let dark_mode = shouldBeDark()
-    setTheme(dark_mode)
 
     // increase time for next alarm by one day
     times[dark_mode ? 1 : 0] += 60 * 60 * 24;
 
-    // create a new alarm
+    // create a new alarm to switch to other theme
     browser.alarms.clearAll();
-    const when = times[dark_mode ? 1 : 0];
+    const when = times[!dark_mode ? 1 : 0];
     browser.alarms.create("auto_dark_mode", {
         when
     });
@@ -50,8 +49,7 @@ function changeTheme() {
     });
 }
 
-function onResponse(response) {
-    console.debug('received response')
+function applyConfig(response) {
     if (!response.enabled) {
         console.log("Yin Yang is disabled")
         return;
@@ -67,21 +65,22 @@ function onResponse(response) {
 
     // if the theme should change automatically:
     times = response.times;
-    changeTheme();
-
-    // add listener for alarms
-    browser.alarms.onAlarm.addListener((alarm) => {
-        if (alarm.name === "auto_dark_mode") {
-            console.log('alarm ' + alarm.name + ' went off');
-            changeTheme();
-        }
-    });
-}
-
-function onError(error) {
-    console.error(`Error: ${error}`);
+    setTheme(shouldBeDark())
+    update_alarm();
 }
 
 // Ask for settings from yin_yang
 console.debug("Loading settings from native application");
-browser.runtime.sendNativeMessage("yin_yang", "GetSettings").then(onResponse, onError);
+browser.runtime.sendNativeMessage("yin_yang", "GetSettings").then(
+    applyConfig,
+    () => console.error(`Error: ${error}`)
+);
+
+// add listener for alarms
+browser.alarms.onAlarm.addListener((alarm) => {
+    if (alarm.name === "auto_dark_mode") {
+        console.log('alarm ' + alarm.name + ' went off');
+        setTheme(shouldBeDark())
+        update_alarm();
+    }
+});
